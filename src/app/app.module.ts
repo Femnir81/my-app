a58a80b7789bb2fd630c63ea37af0cc8002de1b2
Femnir81 <inmydefence81@yahoo.it>
2022-10-31 17:11:18 +0100

    Compito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { MenuNavComponent } from './menu-nav/menu-nav.component';
+import { MainComponent } from './main/main.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchBarComponent,
+    MenuNavComponent,
+    MainComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
